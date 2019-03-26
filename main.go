496785d43bce1bd0bde0f3b2d9afb2b74c188453
Walter Schmidt <wschmidt@finnegans.com.ar>
2019-03-26 14:57:54 -0300

    Se pueden registrar tenants desde go

--- main.go
+++ main.go
@@ -7,17 +7,16 @@ package main
 //var registracionPrueba = Registracion{1, "Walter", "Schmidt", "sebastian.taka@hotmail.com", "1167188487", "Ingenieria en Sistemas de Informacion", "q1q1qq", "Marina", "Olivella", "molivella@xubio.com" ,"Analisis de Sistemas", "", "Facultad Regional Buenos Aires", "Universidad tecnologica nacional", estadoAprobadoID}
 
 func main() {
-  //CrearTablas()
+  CrearTablas()
   var registracion *Registracion
   registrarTenant(registracion)
-  //router := newRouter()
+
+  /*
+  router := newRouter()
   
-  //fmt.Println("Se empieza a escuchar el puerto")
-  //server := http.ListenAndServe(":8081", router)
+  fmt.Println("Se empieza a escuchar el puerto")
+  server := http.ListenAndServe(":8081", router)
 
-  //log.Fatal(server)
-
-  //fmt.Println(err)
-	//defer db.Close()
-
+  log.Fatal(server)
+  */
 }
--- registrarTenant.go
+++ registrarTenant.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -10,17 +11,27 @@ import (
 )
 
 func registrarTenant(registracion *Registracion) {
-	//url := "http://localhost:8080/NOSecure/PUReceiveFeed"
-	url := "https://localhost:8443/CreateCuenta"
-	fmt.Println("URL:>", url)
+	//Direccion := "http://localhost:8080/NOSecure/PUReceiveFeed"
+	Direccion := "https://localhost:8443/CreateCuenta"
+	fmt.Println("URL:>", Direccion)
+	//var valores = url.Values{"key": {"Value"}, "id": {"123"}}
+	//resp, err := http.PostForm(Direccion, valores)
+	var parametros string = fmt.Sprintf("newDominio=%s&newUserName=%s&newPassword=%s&newNombre=%s&newPlan=inicial&newTelefono=%s&newCodigoPromocional=&soyContador=false&soyEmpresa=true&soyEstudiante=true&leiTerminos=true&g-recaptcha-response=03AOLTBLS8pKEId5Nr9_ZSiDl_3OuYqivWEbxtFgGaL-gHbpy6IURAso2e6-eWRYssPWYt4gZ8IT9clVrde8Xsm5j0X_HcCmQhlX34NyWWdUJRQd0OqQvJeZW-D04yBV7obhj7awOyzw8qNLRN3llSW8-87roJE9ixewym9FpYme30QLMxEKOzTdtIlPRjzOtrit4RyY8L4XiznRWhukL6T1fR3W2RB3FUxVMrcCJsgS1mDtILjz6Fc9b317d2oum-NN-FzfSKE8c9SokumoUDi3xCU3iBOO7KSHiWkVRMQX8CKwm3rIJ9irdZRvFZEknmOnvyO91QldaU&pais=AR&ip=190.55.124.44&ip_pais=AR&locationPathname=/NXV/inicio/createAccount.jsp", "dsadsaddsasd2@xaaxsa.com", "dsadsaddsasd2@xaaxsa.com", "12345678", "nombre", "123213123")
+	//var parametros string = fmt.Sprintf("newDominio=%s&newUserName=%s&newPassword=%s&newNombre=%s&newPlan=inicial&newTelefono=%s&newCodigoPromocional=&soyContador=false&soyEmpresa=true&leiTerminos=true&g-recaptcha-response=03AOLTBLS8pKEId5Nr9_ZSiDl_3OuYqivWEbxtFgGaL-gHbpy6IURAso2e6-eWRYssPWYt4gZ8IT9clVrde8Xsm5j0X_HcCmQhlX34NyWWdUJRQd0OqQvJeZW-D04yBV7obhj7awOyzw8qNLRN3llSW8-87roJE9ixewym9FpYme30QLMxEKOzTdtIlPRjzOtrit4RyY8L4XiznRWhukL6T1fR3W2RB3FUxVMrcCJsgS1mDtILjz6Fc9b317d2oum-NN-FzfSKE8c9SokumoUDi3xCU3iBOO7KSHiWkVRMQX8CKwm3rIJ9irdZRvFZEknmOnvyO91QldaU&pais=AR&ip=190.55.124.44&ip_pais=AR&locationPathname=/NXV/inicio/createAccount.jsp", registracion.Email, registracion.Email, registracion.Clave, registracion.Nombre, registracion.Telefono)
+	var urlencodedstring = []byte(parametros)
+	req, err := http.NewRequest("POST", Direccion, bytes.NewBuffer(urlencodedstring))
+	//req.Header.Set("Accept", "*/*")
+	//req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	//req.Header.Set("Accept-Language", "es-419,es;q=0.9")
+	//req.Header.Set("Cache-Control", "no-cache")
+	//req.Header.Set("Connection", "keep-alive")
+	//req.Header.Set("Content-Length", "")
 
-	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-	req.Header.Set("X-Custom-Header", "myvalue")
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
 
 	client := &http.Client{
 		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 			Proxy: http.ProxyFromEnvironment,
 			DialContext: (&net.Dialer{
 				Timeout:   120 * time.Second,
